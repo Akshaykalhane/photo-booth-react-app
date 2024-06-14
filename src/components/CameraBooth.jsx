8d1de21abda390434b331ef834deb63cfddcd727
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-06-14 19:47:51 +0530

    minor changes

--- src/components/CameraBooth.jsx
+++ src/components/CameraBooth.jsx
@@ -3,6 +3,7 @@ import '../App.css';
 import Webcam from 'react-webcam';
 import Button from './Button';
 import Notification from './Notification';
+import html2canvas from 'html2canvas';
 
 function CameraBooth() {
     const imageRef = useRef(null)
@@ -12,34 +13,40 @@ function CameraBooth() {
     const [success, setSuccess] = useState(false);
     const [message, setMessage] = useState('')
 
-    const handleCapture = () => {
+    const handleCapture =async () => {
         const currentImg = imageRef.current.getScreenshot();
         setCurrentImage(currentImg);
         console.log(currentImg)
+        const canvas = await html2canvas(canvasRef.current,{
+            useCORS:true,
+            scale:1,
+        })
+        const imageSrc = canvas.toDataURL('image/png')
+        setCaptureImage(imageSrc)
         // setCaptureImage(imageSrc);
 
         // setSuccess(true);
         // setMessage('Image Captured')
-        const webcamWindow = imageRef.current.video;
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+        // const webcamWindow = imageRef.current.video;
+        // const canvas = canvasRef.current;
+        // const context = canvas.getContext('2d');
 
-        canvas.width=webcamWindow.videoWidth-60;
-        canvas.height=webcamWindow.videoWidth;
-        console.log(canvas.width,canvas.height);
-        //draw image
-        context.drawImage(webcamWindow,0,0,canvas.width,canvas.height)
-        //canvas working
-        const frameImage = new Image();
-        frameImage.src='./images/frame.png';
+        // canvas.width=webcamWindow.videoWidth-60;
+        // canvas.height=webcamWindow.videoWidth;
+        // console.log(canvas.width,canvas.height);
+        // //draw image
+        // context.drawImage(webcamWindow,0,0,canvas.width,canvas.height)
+        // //canvas working
+        // const frameImage = new Image();
+        // frameImage.src='./images/frame.png';
         
-        frameImage.onload=()=>{
-            context.drawImage(frameImage,5,5,canvas.width,canvas.height)
-            const imageSrc= canvas.toDataURL('image/png');
+        // frameImage.onload=()=>{
+        //     context.drawImage(frameImage,0,0,canvas.width,canvas.height)
+        //     const imageSrc= canvas.toDataURL('image/png');
             
-            console.log('image capture')
-            setCaptureImage(imageSrc)
-        }
+        //     console.log('image capture')
+        //     setCaptureImage(imageSrc)
+        // }
     }
     useEffect(() => {
         setTimeout(() => {
@@ -104,7 +111,7 @@ function CameraBooth() {
                     <div className="home-button">
                 <button><i class="fa fa-home"></i> home</button>
                 </div>
-                <div className="web-cam-container">
+                <div className="web-cam-container" ref={canvasRef}>
                     <div className="web-cam-outer">
                         {currentImage ? <img src={currentImage} className='webcam' /> : <Webcam
                             audio={false}
@@ -114,8 +121,8 @@ function CameraBooth() {
                             className='webcam' />}
                     </div>
                     <img src="./images/frame.png" className='overlay-img' alt="" />
+               {/* <canvas ref={canvasRef} style={{display:'none'}}></canvas> */}
                 </div>
-                <canvas ref={canvasRef} style={{display:'none'}}></canvas>
             </div>
             <div className="side-img">
                 <img src="./images/right-img.png" alt="" />
